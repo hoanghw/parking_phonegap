9d882bb659b813064e65149688703391a242263a
Hoang Nguyen <hoanghw@berkeley.edu>
2013-08-13 20:12:41 -0700

    need to test the slider

--- www/js/confirmed.js
+++ www/js/confirmed.js
@@ -1,14 +1,32 @@
 // JavaScript Document
 function confirmed(event){
 	var garageName = event.data.garageName;
-	var text = '<div class="row">'
-			+'You are currently parking at '
-			+garageName
-			+'</div>'
-			+'<div class="row">'
-			+'<input id="check-out-btn" class="btn btn-danger" type="button" value="Check Out"/>'
-			+'</div>'
-			+'<br/>';
-	$('#confirming').modal('hide')
-	$('#content-window').html(text);
+	var rate = event.data.rate;
+	var totalCost = event.data.totalCost;
+    var granularity = event.data.granularity;
+    var duration = event.data.duration;
+	var u = window.localStorage["username"];
+    var today = new Date();
+    var timestamp = today.getTime();
+
+	$.get(SERVER_URL+"ucheckin/", {data:JSON.stringify({garage:garageName,rate:rate,totalCost:totalCost,username:u,timestamp:timestamp,granularity:granularity,duration:duration})}, function(data, textStatus, jqXHR) {
+
+        },"json")
+        .done(function(){
+            $('#confirming').modal('hide');
+	        $('#content-window').html(changeToParked(garageName));
+        })
+        .fail(function(){
+            $('#confirming').modal('hide');
+	        $('#content-window').html(changeToError());
+        });
+}
+
+function signOut(){
+	window.localStorage.removeItem("isLoggedIn");
+	window.location.href= "main.html";
+}
+
+function checkOut(){
+	$('#content-window').html(changeToNotParked());
 }
---
+++ www/js/server.js
@@ -0,0 +1,8 @@
+// Server Properties
+//var SERVER_URL ="http://54.226.45.247/";http://127.0.0.1:8000/
+var SERVER_URL ="http://54.224.3.167/";
+var TIME_OUT = 10000;
+var PER_DAY = 1;
+var PER_HOUR = 2;
+var PER_QUARTER = 3;
+var DEFAULT_DURATION_HOUR = 2;
